6faf654c749cb0948d6841c8bafa2a1857559b3a
artem <artem@DESKTOP-7S1DBR4>
2023-04-26 23:22:46 +0300

    Добавьте файлы проекта.

---
+++ Task7SQLite/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Task7SQLite.Model;
+
+namespace Task7SQLite
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly ServiceProvider serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            services.AddDbContext<ProductDbContext>(p =>
+            {
+                p.UseSqlite("Data Source = Task7.db");
+            });
+            services.AddSingleton<MainWindow>();
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void OnStartUp(object sender, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+}
---
+++ Task7SQLite/Model/ProductDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7SQLite.Model
+{
+    public class ProductDbContext : DbContext
+    {
+        #region Конструктор
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+            : base(options) 
+        { 
+            Database.EnsureCreated();
+        }
+        #endregion
+
+        #region Public свойства
+        public DbSet<Product> Products { get; set;}
+        #endregion
+
+        #region Методы
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(GetProducts());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private Product[] GetProducts() => new Product[]
+        {
+            new Product{ProductID = 1,
+                        ProductName = "Видеокарта KFA2 GeForce RTX 3070 Core",
+                        ProductDescription = "(LHR) [37NSL6MD2KCK] [PCI-E 4.0, 8 ГБ GDDR6, 256 бит, DisplayPort x3, HDMI, GPU 1500 Мгц]",
+                        ProductPrice = 49799,
+                        ProductUnit = 20},
+            new Product{ProductID = 2,
+                        ProductName = "Видеокарта GIGABYTE GeForce RTX 3090 TI GAMING",
+                        ProductDescription = "[GV-N309TGAMING-24GD] [PCI-E, 24 ГБ GDDR6X, 384 бит, DisplayPort x3, HDMI, GPU 1500 Мгц]",
+                        ProductPrice = 99999,
+                        ProductUnit = 18},
+            new Product{ProductID = 3,
+                        ProductName = "Видеокарта Palit GeForce RTX 3060 DUAL OC",
+                        ProductDescription = "(LHR) [NE63060T19K9-190AD] [PCI-E 4.0, 12 ГБ GDDR6, 256 бит, DisplayPort x3, HDMI, GPU 1320 Мгц]",
+                        ProductPrice = 38999,
+                        ProductUnit = 40},
+            new Product{ProductID = 4,
+                        ProductName = "Видеокарта GIGABYTE GeForce RTX 3070 TI GAMING OC",
+                        ProductDescription = "(LHR) [GV-N307TGAMING OC-8GD] [PCI-E 4.0, 8 ГБ GDDR6X, 256 бит, Display x2, HDMI, GPU 1830 Мгц]",
+                        ProductPrice = 70999,
+                        ProductUnit = 52}
+        };
+        #endregion
+    }
+}
